f701255d6dba257bb4edbe545635d5933ed7d26d
Anirudh29Reddy <dontula.anirudh@gmail.com>
2025-02-19 19:37:14 +0530

    Modified cricketstats files and added for new dashboard

--- CricketDashboard-anirudh-main/src/Components/HomeComponents/CricketStats.jsx
+++ CricketDashboard-anirudh-main/src/Components/HomeComponents/CricketStats.jsx
@@ -32,10 +32,10 @@ export default function CricketConnect() {
                     <img src="StstsImage.png" alt="Cricket" className="statistic-image" />
                 </div>
                 <div className="statistic-content">
-                    <h1 className='statistic-content-Heading' style={{color:'black'}}>Connecting Cricketers and Coaches</h1>
-                    <p className='statistic-content-para' style={{color:'black'}}>
+                    <h1 className='statistic-content-Heading'>Connecting Cricketers and Coaches</h1>
+                    <p className='statistic-content-para'>
                     At CricketConnect, we are dedicated to enhancing the performance of cricketers and coaches through our innovative platform. Our mission is to empower users with personalized tools and resources for growth and success.                        </p>
-                    <div className="statistic-stats" style={{color:'black'}}>
+                    <div className="statistic-stats">
                         <div>
                             <h3 className="statistic-count">{users} Users</h3>
                             <p>Join our community of over 5, 000 users who are transforming their cricketing journey.</p>
