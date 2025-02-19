1a300fd78764af3f5716d12b8413f14751104aa1
Anirudh29Reddy <dontula.anirudh@gmail.com>
2025-02-19 16:48:51 +0530

    made reportsandanalysis fro the cricket doashboard

---
+++ CricketDashboard-anirudh-main/src/Components/CricketerDashboardComponents/ReportsAndAnalyze.jsx
@@ -0,0 +1,21 @@
+
+
+
+const ReportsAndAnalyze = () =>{
+ return <>
+ 
+ <div className="row" style={{marginTop:'10%'}}>
+    <div className="col-md-5 text-center">
+        <button className="btn btn-success">Reports</button>
+    </div>
+    <div className="col-md-2"></div>
+    <div className="col-md-5 text-center">
+        <button className="btn btn-success">Analyze Coach</button>
+    </div>
+ </div>
+ 
+ 
+ </>
+}
+
+export default ReportsAndAnalyze;
