4a8375ca6ca748ca789dc0f319eda5e4058148cd
Anirudh29Reddy <dontula.anirudh@gmail.com>
2025-03-13 01:18:05 +0530

    updated ourprocesses in home.js

--- CricketDashboard-anirudh-main/src/pages/Homes.js
+++ CricketDashboard-anirudh-main/src/pages/Homes.js
@@ -1,4 +1,3 @@
-
 import Headers from "../Components/HomeComponents/Headers";
 import HeroSections from "../Components/HomeComponents/HeroSections";
 import OurProcesss from "../Components/HomeComponents/OurProcesss";
@@ -13,36 +12,20 @@ import FooterSection from "../Components/HomeComponents/FooterSection";
 
 export default function Homes() {
 
-  const faqs = [{
-    question : 'whats your doubt',
-    answer : 'this is my answer'
-  }
-  ,
-  {
-    question : 'whats your doubt 2',
-    answer : 'you should say'
-  }
-  ,
-  {
-    question : 'whats your doubt 2',
-    answer : 'you should say'
-  }
-
-]
+  
   return (
     <div>
-      <Headers  />
-      <HeroSections />
-      <CricketStats />
-      <div className="contianer m-1" >
-      {faqs.map((item)=> <OurProcesss value={item}/>)}
-      </div>
-      
-      <FeaturesSection /> 
-      <LatestInsightsSection />
-      <CricketMoments />
-      <ContactSection />
-      <FooterSection />
-    </div>
+    <Headers  />
+    <HeroSections />
+    <CricketStats />
+    <OurProcesss />
+   
+    
+    <FeaturesSection /> 
+    <LatestInsightsSection />
+    <CricketMoments />
+    <ContactSection />
+    <FooterSection />
+  </div>
   );
 }
